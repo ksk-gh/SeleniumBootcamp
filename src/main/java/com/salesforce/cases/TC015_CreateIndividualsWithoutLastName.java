f3516a027020d5325559563bbfe334a4dd6a993c
ksk-gh <ksk.ee25@gmail.com>
2022-09-25 13:13:07 +0530

    updated1

--- src/main/java/com/salesforce/cases/TC015_CreateIndividualsWithoutLastName.java
+++ src/main/java/com/salesforce/cases/TC015_CreateIndividualsWithoutLastName.java
@@ -14,7 +14,7 @@ import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class TC015_CreateIndividualsWithoutLasName {
+public class TC015_CreateIndividualsWithoutLastName {
 	
 		@Test
 		public void tc015(){
